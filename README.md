3bda86511a52e7a3192d8a1f18aec238f7c3e5b9
Dennis Voltz <dennis.voltz@exchange.wwu.de>
2019-08-22 15:24:47 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,85 @@
-# easydb-ulb-plugins
-Plugins made by the ULB for easyDB
+[![Contributors][contributors-shield]][contributors-url]
+[![Forks][forks-shield]][forks-url]
+[![Issues][issues-shield]][issues-url]
+[![MIT License][license-shield]][license-url]
+
+
+<br />
+<p align="center">
+  <a href="https://ulb.uni-muenster.de">
+    <img src="https://www.uni-muenster.de/imperia/md/images/ulb2/_v/logo.svg" alt="Logo">
+  </a>
+
+  <h3 align="center">Plugins for easyDB</h3>
+
+  <p align="center">
+    <br />
+    <br />
+    <a href="https://ulb.uni-muenster.de">Visit ULB</a>
+    ·
+    <a href="https://www.sub.uni-goettingen.de/sub-aktuell/">Visit UB Göttingen</a>
+    ·
+    <a href="https://www.programmfabrik.de/easydb/">Visit easyDB</a>
+  </p>
+</p>
+
+
+## Getting Started
+
+### Prerequisites
+
+* easyDB Instance (with root Privileges)
+* [InvNr Service Instance](https://github.com/ulbmuenster/inventarnummer_service)
+* Proper configured firewall / proxy
+
+### Installation
+
+1. Update your easydb5-master.yml
+
+        extension:
+            plugins:
+            - name: ulb
+                file: plugin/ulb/ulb.yml
+        plugins:
+            enabled+:
+            - extension.ulb
+
+2. Copy ulb.py and ulb.yml into `srv/easydb/config/plugin/ulb`
+3. Restart easyDB Instance via `sudo /srv/easydb/maintain restart`
+
+## Usage
+
+Update a file with objecttype "ztest" and **leave field "invnr" blank**.
+The Plugin will automatically insert a random invnr in this field when saving.
+
+## Logging
+
+Logs can be read via `sudo docker logs easydb-server`
+
+
+## Roadmap
+
+* Insert Configfile, so that other objecttypes can be handled
+* Insert logic, such as, only certain number areas will be returned from the service
+* Writing tests
+
+## Contributing
+
+Contributions are what make the open source community such an amazing place to be learn, inspire, and create. Any contributions you make are **greatly appreciated**.
+
+1. Fork the Project
+2. Create your Feature Branch (`git checkout -b feature/AmazingFeature`)
+3. Commit your Changes (`git commit -m 'Add some AmazingFeature'`)
+4. Push to the Branch (`git push origin feature/AmazingFeature`)
+5. Open a Pull Request
+
+
+## License
+
+Distributed under the MIT License. See `LICENSE` for more information.
+
+
+## Contact
+
+Dennis Voltz - [dennis.voltz@wwu.de](dennis.voltz@wwu.de)
+Adriano Neufend - [neufenda@wwu.de](neufenda@wwu.de)
---
+++ ulb.py
@@ -0,0 +1,80 @@
+# coding=utf8
+
+import os
+import json
+import yaml
+import requests
+
+from datetime import datetime, date
+import calendar
+import locale
+from dateutil.relativedelta import relativedelta
+
+from time import sleep
+from threading import Thread
+
+from context import EasydbException
+from context import InvalidValueError
+from context import get_json_value
+
+
+# called from easydb
+def easydb_server_start(easydb_context):
+    # called when server starts (just once)
+    logger = easydb_context.get_logger('ulb')
+    logger.debug('registering ulb plugin')
+
+    # api callbacks that extend the api
+    easydb_context.register_callback(
+        'db_pre_update', {'callback': 'pre_update'})
+
+
+# method for the 'db_pre_update' callback
+def pre_update(easydb_context, easydb_info):
+    # get a logger
+    logger = easydb_context.get_logger('ulb.pre_update')
+    logger.info("pre_update called via ulb plugin")
+
+    url = "http://example.com"  # TODO insert url of invnr service here
+    jsonwebtoken = "naaaa"  # TODO insert valid jwt here
+    try:
+        result = requests.post(url=url, headers={
+                               'Authorization': 'Bearer '+jsonwebtoken}, json={"institution": "ABC", "prefix": "cfg"})  # TODO configure prefix
+        json_data = result.json()
+        if "id" in json_data:
+            inventarnummer = json_data["id"]
+        else:
+            logger.debug(json.dumps(json_data))
+    except requests.exceptions.ConnectionError:
+        logger.debug("Exception")
+
+    # get the object data
+    data = get_json_value(easydb_info, "data")
+    logger.debug("%d Objects" % len(data))
+
+    # check the data, and if there is invalid data, throw an InvalidValueError
+    for i in range(len(data)):
+
+        # check if the objecttype is set
+        if "_objecttype" not in data[i]:
+            continue
+
+        # check if the objecttype is correct
+        if data[i]["_objecttype"] != "ztest":
+            logger.debug("Ignoring object type %s" % data[i]["_objecttype"])
+            continue
+
+        # to avoid confusion with masks and read/write settings in masks, always use the _all_fields mask
+        data[i]["_mask"] = "_all_fields"
+
+        # only write invnr if field is empty
+        if get_json_value(data[i], "ztest.invnr") is None:
+            try:
+                data[i]["ztest"]["invnr"] = inventarnummer
+            except:
+                logger.debug("Problem generating invnr: " + inventarnummer +
+                             " at object " + get_json_value(data[i], "ztest._id"))
+
+    # always return if no exception was thrown, so the server and frontend are not blocked
+    print json.dumps(data, indent=4)
+    return data
