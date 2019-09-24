c90b9f5eac4709644cd954bc018ceabacbc23a24
neufenda <adriano.neufend@exchange.wwu.de>
2019-09-24 07:34:38 +0200

    Logik zum Ansprechen des Konverters hinzugefügt. Spaces zu Tabs konvertiert.

--- ulb.py
+++ ulb.py
@@ -20,61 +20,73 @@ from context import get_json_value
 
 # called from easydb
 def easydb_server_start(easydb_context):
-    # called when server starts (just once)
-    logger = easydb_context.get_logger('ulb')
-    logger.debug('registering ulb plugin')
+	# called when server starts (just once)
+	logger = easydb_context.get_logger('ulb')
+	logger.debug('registering ulb plugin')
 
-    # api callbacks that extend the api
-    easydb_context.register_callback(
-        'db_pre_update', {'callback': 'pre_update'})
+	# api callbacks that extend the api
+	easydb_context.register_callback(
+		'db_pre_update', {'callback': 'pre_update'})
 
 
 # method for the 'db_pre_update' callback
 def pre_update(easydb_context, easydb_info):
-    # get a logger
-    logger = easydb_context.get_logger('ulb.pre_update')
-    logger.info("pre_update called via ulb plugin")
+	inventarnummer = ''
+	convertedFormula = ''
+	# get a logger
+	logger = easydb_context.get_logger('ulb.pre_update')
+	logger.info("pre_update called via ulb plugin")
 
-    url = "http://example.com"  # TODO insert url of invnr service here
-    jsonwebtoken = "naaaa"  # TODO insert valid jwt here
-    try:
-        result = requests.post(url=url, headers={
-                               'Authorization': 'Bearer '+jsonwebtoken}, json={"institution": "ABC", "prefix": "cfg"})  # TODO configure prefix
-        json_data = result.json()
-        if "id" in json_data:
-            inventarnummer = json_data["id"]
-        else:
-            logger.debug(json.dumps(json_data))
-    except requests.exceptions.ConnectionError:
-        logger.debug("Exception")
+	# get the object data
+	data = get_json_value(easydb_info, "data")
+	logger.debug("%d Objects" % len(data))
 
-    # get the object data
-    data = get_json_value(easydb_info, "data")
-    logger.debug("%d Objects" % len(data))
+	# check the data, and if there is invalid data, throw an InvalidValueError
+	for i in range(len(data)):
 
-    # check the data, and if there is invalid data, throw an InvalidValueError
-    for i in range(len(data)):
+		# check if the objecttype is set
+		if "_objecttype" not in data[i]:
+			continue
 
-        # check if the objecttype is set
-        if "_objecttype" not in data[i]:
-            continue
+		# check if the objecttype is correct
+		if data[i]["_objecttype"] != "invrnr":
+			url = "http://example.com"  # TODO insert url of invnr service here
+			jsonwebtoken = "naaaa"  # TODO insert valid jwt here
+			try:
+				result = requests.post(url=url, headers={
+					'Authorization': 'Bearer ' + jsonwebtoken}, json={"institution": "ABC", "prefix": "cfg"})
+				# TODO configure prefix
+				json_data = result.json()
+				if "invnr" in json_data:
+					inventarnummer = json_data["invnr"]
+				else:
+					logger.debug(json.dumps(json_data))
+			except requests.exceptions.ConnectionError:
+				logger.debug("Exception")
+		if data[i]["_objecttype"] != "formula":
+			url = "http://example.com"  # TODO insert url of converter service here
+			result = requests.post(url=url, json={"formula": "ABC"})  # TODO configure formula sent
+			json_data = result.json()
+			if "convertedFormula" in json_data:
+				convertedFormula = json_data["convertedFormula"]
+			else:
+				logger.debug(json.dumps(json_data))
+		# to avoid confusion with masks and read/write settings in masks, always use the _all_fields mask
+		data[i]["_mask"] = "_all_fields"
 
-        # check if the objecttype is correct
-        if data[i]["_objecttype"] != "ztest":
-            logger.debug("Ignoring object type %s" % data[i]["_objecttype"])
-            continue
-
-        # to avoid confusion with masks and read/write settings in masks, always use the _all_fields mask
-        data[i]["_mask"] = "_all_fields"
-
-        # only write invnr if field is empty
-        if get_json_value(data[i], "ztest.invnr") is None:
-            try:
-                data[i]["ztest"]["invnr"] = inventarnummer
-            except:
-                logger.debug("Problem generating invnr: " + inventarnummer +
-                             " at object " + get_json_value(data[i], "ztest._id"))
-
-    # always return if no exception was thrown, so the server and frontend are not blocked
-    print json.dumps(data, indent=4)
-    return data
+		# only write invnr if field is empty
+		if get_json_value(data[i], "ztest.invnr") is None:
+			try:
+				data[i]["ztest"]["invnr"] = inventarnummer
+			except:
+				logger.debug("Problem generating invnr: " + inventarnummer +
+							 " at object " + get_json_value(data[i], "ztest._id"))
+		if get_json_value(data[i], "ztest.formula") is None:
+			try:
+				data[i]["ztest"]["formula"] = convertedFormula
+			except:
+				logger.debug("Problem saving formula: " + convertedFormula +
+							 " at object " + get_json_value(data[i], "ztest._id"))
+	# always return if no exception was thrown, so the server and frontend are not blocked
+	print(json.dumps(data, indent=4))
+	return data
