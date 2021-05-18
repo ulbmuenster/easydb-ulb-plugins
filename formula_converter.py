b32958e433735f8064a14b17f5ce0c07698f7e80
Adriano Neufend <neufenda@uni-muenster.de>
2021-05-18 11:08:28 +0200

    Update Never use "Rename all" function.

--- formula_converter.py
+++ formula_converter.py
@@ -6,6 +6,7 @@ import yaml
 import requests
 
 import sys
+
 reload(sys)
 sys.setdefaultencoding('utf-8')
 import string
@@ -23,55 +24,53 @@ from context import InvalidValueError
 from context import get_json_value
 
 
-
-
 # called from easydb
 def easydb_server_start(easydb_context):
-	# called when server starts (just once)
-	logger = easydb_context.get_logger('ulb')
-	logger.debug('registering ulb plugin')
+    # called when server starts (just once)
+    logger = easydb_context.get_logger('ulb')
+    logger.debug('registering ulb plugin')
 
-	# api callbacks that extend the api
-	easydb_context.register_callback(
-		'db_pre_update', {'callback': 'pre_update'})
+    # api callbacks that extend the api
+    easydb_context.register_callback(
+        'db_pre_update', {'callback': 'pre_update'})
 
 
 # method for the 'db_pre_update' callback
 def pre_update(easydb_context, easydb_info):
-	convertedformula = ''
-	#datamodel/"Datenmodell"
-	datamodel = "fb14_basis_dm_1"
-	# get a logger
-	logger = easydb_context.get_logger('ulb.pre_update')
-	logger.info("pre_update called via formula_converter plugin")
+    convertedformula = ''
+    # datamodel/"Datenmodell"
+    datamodel = "fb14_basis_dm_1"
+    # get a logger
+    logger = easydb_context.get_logger('ulb.pre_update')
+    logger.info("pre_update called via formula_converter plugin")
 
-	# get the object data
-	data = get_json_value(easydb_info, "data")
-	logger.debug("%d Objects" % len(data))
+    # get the object data
+    data = get_json_value(easydb_info, "data")
+    logger.debug("%d Objects" % len(data))
 
-	# check the data, and if there is invalid data, throw an InvalidValueError
-	for i in range(len(data)):
-		# check if datamodel is in data
-		if datamodel not in data[i]:
-			continue
-		# check if formel is in mineralogie
-		if "formel" not in data[i][datamodel]:
-			continue
+    # check the data, and if there is invalid data, throw an InvalidValueError
+    for i in range(len(data)):
+        # check if datamodel is in data
+        if datamodel not in data[i]:
+            continue
+        # check if formel is in datamodel
+        if "formel" not in data[i][datamodel]:
+            continue
 
-		formula = data[i][datamodel]["formel"]
-		url = "https://easydbwebservice.uni-muenster.de/convert"
-		result = requests.post(url=url, json={"formula": formula})
-		json_data = result.json()
-		if "convertedformula" in json_data:
-			convertedformula = json_data["convertedFormula"]
-		else:
-			logger.debug(json.dumps(json_data))
-		# to avoid confusion with masks and read/write settings in masks, always use the _all_fields mask
-		data[i]["_mask"] = "_all_fields"
-		try:
-			data[i][datamodel]["formel"] = convertedformula
-		except:
-			logger.debug("Problem saving formula: " + convertedformula)
-	# always return if no exception was thrown, so the server and frontend are not blocked
-	print(json.dumps(data, indent=4))
-	return data
+        formula = data[i][datamodel]["formel"]
+        url = "https://easydbwebservice.uni-muenster.de/convert"
+        result = requests.post(url=url, json={"formula": formula})
+        json_data = result.json()
+        if "convertedFormula" in json_data:
+            convertedformula = json_data["convertedFormula"]
+        else:
+            logger.debug(json.dumps(json_data))
+        # to avoid confusion with masks and read/write settings in masks, always use the _all_fields mask
+        data[i]["_mask"] = "_all_fields"
+        try:
+            data[i][datamodel]["formel"] = convertedformula
+        except:
+            logger.debug("Problem saving formula: " + convertedformula)
+    # always return if no exception was thrown, so the server and frontend are not blocked
+    print(json.dumps(data, indent=4))
+    return data
